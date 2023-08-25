521eab6619e4e87b9191ad9ca81765b85209744e
Junzhe Wu <junzhewu@umich.edu>
2023-08-25 01:29:14 -0400

    update scripts

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,2 +1,2 @@
 docker build -t tccoin/simple_slam_loop_closure .
-#docker build --progress=plain -t tccoin/simple_slam_loop_closure .
+#docker build --progress=plain -t tccoin/simple_slam_loop_closure . &> build.log
